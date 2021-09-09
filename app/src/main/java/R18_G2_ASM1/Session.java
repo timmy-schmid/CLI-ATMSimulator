072ead7b215957087aaf0d7a8482db446c33305e
Wei Zhang <wzha2184@uni.sydney.edu.au>
2021-09-09 17:42:24 +0800

    init the Session.java

---
+++ app/src/main/java/R18_G2_ASM1/Session.java
@@ -0,0 +1,37 @@
+package R18_G2_ASM1;
+
+public class Session {
+    private ATM attachedATM;
+    private int sessionID;
+    private SessionStatusCode currentStatus;
+    private Transaction transaction;
+    private Card card;
+    private int pinAttemptNum;
+
+    public void run(int cardNum){
+
+    }
+
+    public LIst<Transaction> getTransactions(){
+        return null;
+    }
+
+    public StatusCode validateSession(){
+        return null;
+    }
+
+    public List<Card> retrieveCardDB(){
+        return null;
+    }
+
+    public void transact(Account a){
+
+    }
+
+    public void checkPIN(){
+        
+    }
+
+
+
+}
