fce222f2304512fd073397712e44185061e0245e
Tim <tsch5702@uni.sydney.edu.au>
2021-09-10 20:10:00 +1000

    adding enum implementation

---
+++ app/src/main/java/R18_G2_ASM1/SessionStatus.java
@@ -0,0 +1,26 @@
+package R18_G2_ASM1;
+
+public enum SessionStatus {
+  IN_PROGRESS("The session is still in progress"),
+  INVALID_CARD_NUMBER("The card number does not match a valid card"),
+  CARD_NOT_ACTIVE("The card entered is only active from x date"),
+  CARD_EXPIRED("The card entered has expired"),
+  CARD_LOST("The card entered has been reported as lost/stolen"),
+  CARD_BLOCKED("The card entered has been blocked due to too many PIN attempts"),
+  SUCCESS("The session was succesfully completed");
+
+  private String description;
+
+  private SessionStatus(String description) {
+    this.description = description;
+  }
+
+  public void updateDescription (String s) {
+    description = s;
+  }
+
+  @Override
+  public String toString() {
+    return description;
+  }
+}
