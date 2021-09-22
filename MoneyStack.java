64602f439eddb1fcf830db0da0fc21a3470a05e7
Ke Xu <kexu4389@uni.sydney.edu.au>
2021-09-22 22:45:46 +0800

    update withdraw method, replace double with BigDecimal for higher precision.

--- MoneyStack.java
+++ MoneyStack.java
@@ -1,8 +1,6 @@
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.io.IOException;
+import java.math.BigDecimal;
 
 public class MoneyStack{
     private Map<MoneyType, double> money;
@@ -35,6 +33,16 @@ public class MoneyStack{
         return totalMoney;
     }
 
+    //overload for BigDecimal calculation
+    public BigDecimal totalMoney() {
+        BigDecimal totalMoney = new BigDecimal(0);
+        for(MoneyType T: money.keySet()){
+            BigDecimal add_num = BigDecimal.valueOf(T.getValue()*money.get(T));
+            totalMoney = totalMoney.add(add_num);
+        }
+        return totalMoney;
+    }
+
     public static void addMoney(MoneyType m, int amount) throws IOException{
        if(money.containsKey(m) == false){
            System.out.println("Error: money type doesn't exist");
@@ -46,24 +54,29 @@ public class MoneyStack{
     }
 
     public boolean withdraw(MoneyStack c){
-        int needWithdraw = c.totalMoney();
+        BigDecimal needWithdraw = c.totalMoney();
         if(money.canWithdraw(c) == false){
             return false;
         }else{
-            for(MoneyType key: this.money.keySet()){
-                int quotient = needWithdraw/key.getValue();
-                int remainder  = needWithdraw%key.getValue();
-                if(quotient > money.get(key)){
-                    needWithdraw -= money.get(key)*key.getValue();
+            for(MoneyType key: this.getMoney().keySet()){
+                BigDecimal notesValue = BigDecimal.valueOf(key.getValue());
+                BigDecimal quotient = needWithdraw.divide(notesValue);
+                int intQuotient =  quotient.intValue();
+                quotient = BigDecimal.valueOf(intQuotient);
+                BigDecimal remainder = needWithdraw.subtract(notesValue.multiply(BigDecimal.valueOf(intQuotient)));
+                BigDecimal notesAmount = new BigDecimal(money.get(key));
+                if(notesAmount.compareTo(BigDecimal.valueOf(intQuotient)) == 0 || notesAmount.compareTo(BigDecimal.valueOf(intQuotient)) == -1){
+                    BigDecimal alreadyCal = notesAmount.multiply(notesValue);
+                    needWithdraw = needWithdraw.subtract(alreadyCal) ;
                     money.replace(key,0);
                 }else{
                     int originalAmount = money.get(key);
                     needWithdraw = remainder;
-                    money.replace(key,originalAmount-quotient);
+                    money.replace(key,(int)originalAmount-intQuotient);
                 }
             }
-            return (needWithdraw == 0);
         }
+        return true;
     }
 
     public int query(MoneyType c) throws IOExeception{
@@ -75,20 +88,26 @@ public class MoneyStack{
     }
 
     private boolean canWithdraw(MoneyStack c){
-        int needWithdraw = c.totalMoney();
-        if(needWithdraw > this.money.totalMoney()){
+        BigDecimal needWithdraw = c.totalMoney();
+        if (needWithdraw.compareTo(this.totalMoney()) == 1){ //change this.money --> this?
             return false;
-        }else{
-            for(MoneyType key: this.money.keySet()){
-                int quotient = needWithdraw/key.getValue();
-                int remainder = needWithdraw%key.getValue();
-                if(quotient > money.get(key)){
-                    needWithdraw -= money.get(key)*key.getValue();
+        } else{
+            for(MoneyType key: this.getMoney().keySet()){
+                BigDecimal notesValue = BigDecimal.valueOf(key.getValue());
+                System.out.println(notesValue);
+                BigDecimal quotient = needWithdraw.divide(notesValue);
+                int intQuotient =  quotient.intValue();
+                quotient = BigDecimal.valueOf(intQuotient);
+                BigDecimal remainder = needWithdraw.subtract(notesValue.multiply(quotient));
+                BigDecimal notesAmount = BigDecimal.valueOf(money.get(key));
+                if(notesAmount.compareTo(BigDecimal.valueOf(intQuotient)) == 0 || notesAmount.compareTo(BigDecimal.valueOf(intQuotient)) == -1){
+                    BigDecimal alreadyCal = notesAmount.multiply(notesValue);
+                    needWithdraw = needWithdraw.subtract(alreadyCal) ;
                 }else{
                     needWithdraw = remainder ;
                 }
             }
-            return (needWithdraw == 0);
+            return (needWithdraw.compareTo(BigDecimal.valueOf(0)) == 0);
         }
     }
 
@@ -103,5 +122,37 @@ public class MoneyStack{
     public boolean getstatusOfMoney(){
         return this.statuOfMoney;
     }
+    //newly added functions below xdddd
+
+    public HashMap <MoneyType, Integer> getMoney(){
+        return this.money;
+    }
+
+    //probably need to create new functions for this (ATM cash dispenser)??
+    /*
+        addMoneyStack(MoneyStack m) --> add to current moneystack
+        balance.withdraw(MoneyStack m) --> for ejectMoney
+    */
+    // should it be boolean or void?
+    public void addMoneyStack(MoneyStack m){
+        for(Map.Entry<MoneyType, Integer> ATMEntry: this.getMoney().entrySet()){
+            for(Map.Entry<MoneyType, Integer> Addentry: m.getMoney().entrySet()){
+                if(ATMEntry.getKey().equals(Addentry.getKey())){
+                    this.getMoney().replace(ATMEntry.getKey(),ATMEntry.getValue()+Addentry.getValue());
+                }
+            }
+        }
+    }
+
+    public boolean withdrawStack(MoneyStack m){
+        for(Map.Entry<MoneyType, Integer> ATMEntry: this.getMoney().entrySet()){
+            for(Map.Entry<MoneyType, Integer> Addentry: m.getMoney().entrySet()){
+                if(ATMEntry.getKey().equals(Addentry.getKey())){
+                    if (ATMEntry.getValue()<Addentry.getValue()){return false;}
+                    else {this.getMoney().replace(ATMEntry.getKey(),ATMEntry.getValue()-Addentry.getValue());}
+                }
+            }
+        }
+        return true;}
 
 }
