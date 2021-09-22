0710a773212deefa651090284c9d9216839db7c3
Wei Zhang <wzha2184@uni.sydney.edu.au>
2021-09-22 22:51:57 +0800

    Session Test

--- app/src/test/java/R18_G2_ASM1/SessionTest.java
+++ app/src/test/java/R18_G2_ASM1/SessionTest.java
@@ -3,54 +3,72 @@ import org.junit.jupiter.api.Test;
 
 import R18_G2_ASM1.Session.InvalidTypeException;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import org.junit.jupiter.api.BeforeEach;
 
 
 public class SessionTest {
+    File csvCardTest;
+    Session section;
+    Card card;
+    DateFormat dateFormat;
+    SimpleDateFormat simpleDateFormat;
+
     @BeforeEach
     public void setUp() throws InvalidTypeException, IOException{
 
-        File csvCardTest;
-        File csvCard;
-        String s = this.getClass().getResource("/").getPath();
-        System.out.println(s);
-        s = s.substring(0,s.length()-13);
-        System.out.println(s);
 
-        csvCardTest = new File(s+"app/src/test/datasets/cardTest.csv");
-        csvCard = new File(s+"app/src/main/datasets/card.csv");
+        // String s = this.getClass().getResource("/").getPath();
+        // System.out.println(s);
+        // s = s.substring(0,s.length()-13);
+        // System.out.println(s);
 
+        // csvCardTest = new File(s+"app/src/test/datasets/cardTest.csv");
+        // csvCard = new File(s+"app/src/main/datasets/card.csv");
 
+        // File directory = new File("");//参数为空 
+        // String courseFile = directory.getCanonicalPath() ; 
+        // System.out.println(courseFile); 
 
+        // System.out.println(csvCardTest.exists());
 
-        File directory = new File("");//参数为空 
-        String courseFile = directory.getCanonicalPath() ; 
-        System.out.println(courseFile); 
+        csvCardTest = new File("src/test/datasets/cardTest.csv");
+        String pattern = "yyyy-MM-dd";
+        simpleDateFormat = new SimpleDateFormat(pattern);
 
-        System.out.println(csvCardTest.exists());
+        section = new Session(new ATM("loc"));
+        card = section.retrieveCardFromFile(12345, csvCardTest);
 
-        Session sec = new Session(new ATM("loc"));
-        Card card = sec.retrieveCardFromFile(12345, csvCard);
-        System.out.println(card.getCardNumber());
-        System.out.println(card.getPin());
-        System.out.println(card.getbalance());
+    }
+    @Test
+    void retrieveCardFromFileTest() throws InvalidTypeException, IOException, ParseException{
+        assertEquals(12345, card.getCardNumber(),"correctly read the card number");
+        assertEquals(simpleDateFormat.parse("2017-01-01"), card.getStart_date(),"correctly read the card start date");
+        assertEquals(simpleDateFormat.parse("2027-01-01"), card.getExpiration_date(),"correctly read the expiration date");
+        assertEquals(false, card.is_lost(),"correctly read the card information");
+        assertEquals(false, card.is_blocked(),"correctly read the card information");
+        assertEquals(1234, card.getPin(),"correctly read the card information");
+        assertEquals(90.0, card.getbalance(),"correctly read the card balance");
+        assertEquals("customer", section.getUserType(),"correctly read the card information");
     }
 
-    public static void main(String[] args) throws InvalidTypeException, IOException{
+    // public static void main(String[] args) throws InvalidTypeException, IOException{
 
-        SessionTest st = new SessionTest();
-        st.setUp();
-
-
-        // Session sec = new Session(new ATM("loc"));
-        // Card card = sec.retrieveCardFromFile(11111, csvCard);
-        // System.out.println(card);
-        //System.out.println(card.getAccount());
+    //     SessionTest st = new SessionTest();
+    //     st.setUp();
+    //     // Session sec = new Session(new ATM("loc"));
+    //     // Card card = sec.retrieveCardFromFile(11111, csvCard);
+    //     // System.out.println(card);
+    //     //System.out.println(card.getAccount());
 
         
-    }
+    // }
 
 
     
